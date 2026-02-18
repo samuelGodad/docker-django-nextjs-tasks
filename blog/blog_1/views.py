17cf2cc73aaa1c69773a54c5d42ae4cede7027cb
samuelGodad <godadsamuel@gmail.com>
2026-02-18 11:25:01 +0300

    Add postgres search vector to seach in multiple files

--- blog/blog_1/views.py
+++ blog/blog_1/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 
 from django.views.decorators.http import require_POST
@@ -13,6 +13,7 @@ from django.db.models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from taggit.models import Tag
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_lists(request, tag_slug=None):
@@ -66,19 +67,21 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     form = CommentForm()
     post_tags_ids = post.tags.values_list("id", flat=True)
-    similar_posts = Post.published.filter(
-        tags__in=post_tags_ids
-    ).exclude(id=post.id)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
 
     # Find similar posts: annotate with number of shared tags and order by that, then by publish date
-    similar_posts = (
-        similar_posts.annotate(same_tags=Count("tags"))
-        .order_by("-same_tags", "-publish")[:4]
-    )
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
     return render(
         request,
         "blog/post/post_detail.html",
-        {"post": post, "comments": comments, "form": form,'similar_posts':similar_posts},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
@@ -129,4 +132,28 @@ def post_comment(request, post_id):
         request,
         "blog/post/post_comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            search_vector = SearchVector("title", "body")
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    search=search_vector, rank=SearchRank(search_vector, search_query)
+                )
+                .filter(search=search_query)
+                .order_by("-rank")
+            )
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
